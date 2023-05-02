e420210f33b8c385a948125aeda649ff5f053662
hayato <31178172+hayato0810@users.noreply.github.com>
2023-05-02 17:18:48 +0900

    add spring project

---
+++ src/main/kotlin/com/example/summary/SummaryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.summary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SummaryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SummaryApplication>(*args)
+}
